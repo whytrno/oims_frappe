4eb49cf556b340682aad2b3e12470aae71efeb2b
Wahyu Triono <whytrno@gmail.com>
2024-10-23 11:37:41 +0700

    Fix: Change login logo to oims

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,8 +3,8 @@
 		<ion-content class="ion-padding">
 			<div class="flex h-screen w-screen flex-col justify-center bg-white">
 				<div class="flex flex-col mx-auto gap-3 items-center">
-					<FrappeHRLogo class="h-8 w-8" />
-					<div class="text-3xl font-semibold text-gray-900 text-center">Login to OIMS</div>
+					<img src="/src/logo.png" alt="OIMS Logo" class="h-8 w-8">
+					<!-- <div class="text-3xl font-semibold text-gray-900 text-center">Login to OIMS</div> -->
 				</div>
 
 				<div class="mx-auto mt-10 w-full px-8 sm:w-96">
