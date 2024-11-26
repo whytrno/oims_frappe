e9c7f3130fa658ca129fe7fff8c8f3b40dc8b15b
Wahyu Triono <whytrno@gmail.com>
2024-11-26 07:43:47 +0700

    Feat: Change default map view to center of indonesia

--- oims/public/js/oims.js
+++ oims/public/js/oims.js
@@ -12,3 +12,19 @@ frappe.call('frappe.desk.desktop.get_workspace_sidebar_items')
 			});
 		}
 	});
+
+	// Make sure we have a dictionary to add our custom settings
+const map_settings = frappe.provide("frappe.utils.map_defaults");
+
+// Center and zoomlevel can be copied from the URL of
+// the map view at openstreetmap.org.
+
+// New default location (middle of germany).
+map_settings.center = [-2.5, 118.0]; // Pusat geografis Indonesia
+map_settings.zoom = 5; // Zoom level untuk mencakup seluruh wilayah Indonesia
+
+
+// Use a different map: satellite instead of streets
+// Examples can be found at https://leaflet-extras.github.io/leaflet-providers/preview/
+// map_settings.tiles = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}";
+// map_settings.attribution = "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community";
