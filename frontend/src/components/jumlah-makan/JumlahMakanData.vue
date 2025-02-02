2908b53af0abf4f458a56370c6fd1b4aaf4605c8
Wahyu Triono <whytrno@gmail.com>
2025-02-02 22:43:05 +0700

    Fix: Terdapat data jumlah makan yang tertutup bottom bar

--- frontend/src/components/jumlah-makan/JumlahMakanData.vue
+++ frontend/src/components/jumlah-makan/JumlahMakanData.vue
@@ -1,11 +1,11 @@
 <template>
-	<div class="flex flex-col gap-2.5 mt-5 overflow-auto">
+	<div class="flex flex-col gap-2.5 mt-5 overflow-auto mb-5">
 		<JatahMakanItem v-for="item in items" :key="item.id" :item="item" />
 	</div>
 </template>
 
 <script setup>
-import JatahMakanItem from "./JumlahMakanItem.vue";
+import JatahMakanItem from "./JumlahMakanItem.vue"
 
 const props = defineProps({
 	items: {
